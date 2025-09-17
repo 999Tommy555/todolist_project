8545b8da8c854db08816af3317148c5a84239e94
dading_global <dading_global@gmail.com>
2025-09-17 21:38:21 +0800

    完成登錄功能

--- user/urls.py
+++ user/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import user_register
+from .views import user_register, user_login
 
 urlpatterns = [
-    path("register/", user_register, name="register"),
+    path("login/", user_login, name="user-login"),
+    path("register/", user_register, name="user-register"),
 ]
